a089e6a977a76f1363e7453be863d308c4fef1dd
root <root@ost.dev.eggpire.com>
2012-10-13 16:04:15 -0400

    implemented completely new staff login system create abstract session handling library extended that library to staff and client setup client login harness

---
+++ example/lib/staff_session.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once(ROOT.'/lib/session.php');
+
+class StaffSession extends Session {
+
+	public static function requireManager(){
+		if(self::get('is_manager')) return true;
+		throw new Exception('Permission denied');
+	}
+	
+	public static function init(){
+		self::$config_name = 'staff';
+		self::$session_name = 'staff_token';
+		self::$session_table = 'staff_session';
+		self::$user_primary_key = 'staff_id';
+	}
+	
+}
+
+//overrides the parent vars
+StaffSession::init();
