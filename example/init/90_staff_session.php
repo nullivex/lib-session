b441c825c73b8c0dd3a16894335b131a4db21d01
Nullivex <contact@nullivex.com>
2013-03-12 22:36:07 -0700

    refactored func.php into packages, renamed functions, updated error handling, upgraded boot process

---
+++ example/init/90_staff_session.php
@@ -0,0 +1,26 @@
+<?php
+lib('staff','staff_session');
+
+//check for session
+try {
+	if(StaffSession::checkLogin()){
+		//register session
+		$token = StaffSession::getByToken(StaffSession::getTokenFromSession());
+		$session = array_merge(Staff::get($token['staff_id']),$token);
+		StaffSession::storeSession($session);
+		unset($session,$token);
+		//set tpl globals (if Tpl is available)
+		if(is_callable(array('Tpl','_get'))){
+			Tpl::_get()->set(array(
+				 'staff_name'		=>	StaffSession::get('name')
+				,'staff_lastlogin'	=>	date(Config::get('date','general_format'),StaffSession::get('last_login'))
+			));
+		}
+	} else {
+		if(server('REQUEST_URI') != Url::login()) redirect(Url::login());
+	}
+} catch(Exception $e){
+	StaffSession::tokenDestroy(StaffSession::getTokenFromSession());
+	StaffSession::destroySession();
+	redirect(Url::login());
+}
